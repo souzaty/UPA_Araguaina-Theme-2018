214c8e91dfe1018029cf7c32c06b4e700b9e82f5
Thiago <souza.ty@gmail.com>
2018-03-08 11:56:29 -0300

    Add Files

---
+++ functions/menu.php
@@ -0,0 +1,14 @@
+<?php
+/** Register Theme Menus
+ * @package WordPress
+ * @subpackage UPA 24hrs
+ * @since UPA 24hrs 1.0
+ */
+
+register_nav_menus(array(
+				'menu_principal_heelj' => __('Menu principal'),
+				'menu_principal_modal_ohospital_heelj_c1' => __('Menu modal o hospital col 1'),
+				'menu_principal_modal_ohospital_heelj_c2' => __('Menu modal o hospital col 2'),
+				'menu_principal_modal_ohospital_heelj_c3' => __('Menu modal o hospital col 3'),
+
+));
